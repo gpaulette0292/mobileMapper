ba4600f9669b8848615c5553aa22e9e99bf03da6
Grace Paulette <gpaulette0292@jh-mba158-15.local>
2019-03-11 11:27:30 -0500

    added guard and annotation for title and coordinates

--- mobileMapper/ViewController.swift
+++ mobileMapper/ViewController.swift
@@ -13,6 +13,7 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     let locationManager = CLLocationManager ()
     var currentLocation: CLLocation!
+    var parks : [MKMapItem] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,5 +31,32 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
         print(currentLocation)
     }
 
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        let center = currentLocation.coordinate
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        let region = MKCoordinateRegion(center: center, span: span)
+        mapView.setRegion(region, animated: true)
+    
+    }
+    
+    @IBAction func whenSearchButtonPressed(_ sender: Any) {
+        let request = MKLocalSearch.Request()
+        request.naturalLanguageQuery = "Parks"
+        let span = MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
+        request.region = MKCoordinateRegion(center: currentLocation.coordinate, span: span)
+        let search = MKLocalSearch(request: request)
+        search.start { (response, error ) in
+            guard let response = response else {
+                return
+            }
+            for mapItem in response.mapItems {
+                self.parks.append(mapItem)
+                let annotation = MKPointAnnotation ()
+                annotation.coordinate = mapItem.placemark.coordinate
+                annotation.title = mapItem.name
+                self.mapView.addAnnotation(annotation)
+            }
+        }
 }
 
+}
